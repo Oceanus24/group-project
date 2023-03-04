5f9e9295192003d6622a6de244b08a9c9fc63360
Oceanus24 <124618767+Oceanus24@users.noreply.github.com>
2023-03-04 14:13:36 -0500

    Updates to Index and Page Layout
    -Removed default index and privacy pages.
    -Moved all Ticket pages to main folder.
    -Removed links in banner and updated links.

---
+++ RazorPagesTitanHelpDesk/Pages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesTitanHelpDesk.Data;
+using RazorPagesTitanHelpDesk.Models;
+
+namespace RazorPagesTitanHelpDesk.Pages.Tickets
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesTitanHelpDesk.Data.RazorPagesTitanHelpDeskContext _context;
+
+        public CreateModel(RazorPagesTitanHelpDesk.Data.RazorPagesTitanHelpDeskContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Ticket Ticket { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Ticket == null || Ticket == null)
+            {
+                return Page();
+            }
+
+            _context.Ticket.Add(Ticket);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesTitanHelpDesk/Pages/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTitanHelpDesk.Data;
+using RazorPagesTitanHelpDesk.Models;
+
+namespace RazorPagesTitanHelpDesk.Pages.Tickets
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesTitanHelpDesk.Data.RazorPagesTitanHelpDeskContext _context;
+
+        public DetailsModel(RazorPagesTitanHelpDesk.Data.RazorPagesTitanHelpDeskContext context)
+        {
+            _context = context;
+        }
+
+      public Ticket Ticket { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Ticket == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Ticket.FirstOrDefaultAsync(m => m.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Ticket = ticket;
+            }
+            return Page();
+        }
+    }
+}
