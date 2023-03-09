baa6f09de56c9d2d28138e957bdd3805eb68c73f
Khang Nguyen <knguye37@live.spcollege.edu>
2023-03-09 17:09:10 -0500

    Final

--- RazorPagesTitanHelpDesk/Models/Ticket.cs
+++ RazorPagesTitanHelpDesk/Models/Ticket.cs
@@ -5,11 +5,16 @@ namespace RazorPagesTitanHelpDesk.Models
     public class Ticket
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Name { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime Date { get; set; }
 
         [Display(Name = "Problem Description")]
+        [Required]
         public string? ProblemDescription { get; set; }
     }
 }
